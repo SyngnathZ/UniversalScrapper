7cc0ad02adef7f66641fd7b0af7143d4318b97ac
HJH <Hei.jinghui@gmail.com>
2022-01-18 21:02:14 +0800

    加入IMDB对未查询到的结果进行在判断逻辑

--- MoviesManager.py
+++ MoviesManager.py
@@ -5,6 +5,8 @@ import re
 import getopt
 import sys
 
+from getFromTMDB import getMVFromTMDB
+
 
 def get_allMV(dir):
     print('开始获取待修改的电影...')
@@ -12,15 +14,15 @@ def get_allMV(dir):
         for name in fileNames:
             ext = ['.mp4', '.mkv']
             if name.endswith(tuple(ext)):
-                # print(fileNames)
                 incorrectformat_MV(parent, name)
 
 
 def incorrectformat_MV(dir, filename):
+    movie = {'name': '', 'year': ''}  # 初始化一个字典便于后续处理
     namelist = []
     namelist.append(filename.split('.'))  # 将文件名按照.符号进行分割
 
-    if len(namelist[0][0].split(' ')) >= 2:  # 如果文件名是用空格分隔
+    if len(namelist[0][0].split(' ')) >= 2:  # 如果文件名的第一项用空格分割是个长度超过1的list，那么则可以认为该文件用空格分割
         namelist[0][0] = filename.split(' ')  # 将文件名按照空格进行分割
         namelist[0][0][-1] = namelist[0][0][-1][:-4]  # 删除被分割出来的文件格式名
         namelist[0][0].append(filename[-3:])  # 将最后后缀加上文件格式名
@@ -33,18 +35,18 @@ def incorrectformat_MV(dir, filename):
     for l in namelist[0]:
         match = re.match(r'.*([1-3][0-9]{3})', l)  # 匹配文件名中的年份
         if match is not None:
-            break
+            movie['name'] = " ".join(namelist[0][:i])  # 默认年份前一个元素为名称
+            movie['year'] = namelist[0][i]
+            newname = getMVFromTMDB(movie['name'], movie['year'])
+            if newname != 'NOTFOUND':  # 若找到了新的名字
+                del namelist[0][i + 1:-1]
+                del namelist[0][:i]
+                namelist[0].insert(0, newname)
+                break
         else:
             i += 1
 
-    if match is not None:
-        for l in namelist[0][i + 1:-1]:
-            if namelist[0][i + 1][0] == '[' and namelist[0][i + 1][-1] == ']':
-                break
-            namelist[0][i + 1] = '[' + l + ']'
-            i += 1
-
-        replace_filename(dir, filename, filename, ".".join(namelist[0]))  # 进入更名步骤
+    replace_filename(dir, filename, filename, ".".join(namelist[0]))  # 进入更名步骤
 
 
 def replace_filename(dir, file_name, oldPartName, newPartName, Mode=True):
--- getFromTMDB.py
+++ getFromTMDB.py
@@ -1,9 +1,11 @@
 from tmdbv3api import TV
+
+from imdb import IMDb
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 
 tmdb = TMDb()
-tmdb.api_key = 'YOUR TOKEN'
+tmdb.api_key = 'YOUR API'
 tmdb.language = 'zh'
 
 
@@ -13,8 +15,29 @@ def getMVFromTMDB(name, year):
 
     for res in search:
         try:
-            if res.release_date[:4] == year:  # 通过预知电影年份进行匹配
-                print(res.title)
-                break
+            if res.release_date != '':
+                if int(year) + 1 >= int(res.release_date[:4]) >= int(
+                        year) - 1:  # 通过预知电影年份进行匹配
+                    print(res.title)
+                    return res.title
         except:
             print('没找到，使用其他方法继续查询......')
+            return None
+
+    IMDBid = getMVFromIMDB(name, year)
+    search = movie.external(IMDBid, 'imdb_id')  # 输入电影名查询
+    return search.movie_results[0].title
+
+
+def getMVFromIMDB(name, year):
+    imdb = IMDb()
+    search = imdb.search_movie(name)
+
+    try:
+        for res in search:
+            if res.data['kind'] == 'movie':
+                if int(year) + 1 >= int(res.data['year']) >= int(year) - 1:  # 通过预知电影年份进行匹配
+                    return 'tt' + res.movieID
+    except:
+        print('没找到，使用其他方法继续查询......')
+        return None
