ec35f4832aeb2880f325cb10d1d45c42b1620b89
HJH <Hei.jinghui@gmail.com>
2022-01-29 23:10:38 +0800

    加入对.ts视频格式的支持

--- TVsManager.py
+++ TVsManager.py
@@ -12,7 +12,7 @@ def get_allTV(dir):
     for tvlist in os.listdir(dir):
         medianame = []
         for each in os.listdir(os.path.join(dir, tvlist)):  # 获取二级目录下的视频文件
-            mediaext = ['.mkv', '.mp4', '.ass', '.srt']  # 利用二级目录下的视频文件进行搜索（同时操作字幕）
+            mediaext = ['.mkv', '.mp4', '.ts', '.ass', '.srt']  # 利用二级目录下的视频文件进行搜索（同时操作字幕）
             if each.endswith(tuple(mediaext)):  # 获取二级目录下的nfo文件
                 medianame.append(each)
         find_adj_TVwithskip(medianame, tvlist, dir)  # 获取失败的剧集内容首先跳过处理
@@ -20,7 +20,7 @@ def get_allTV(dir):
     for tvlist in os.listdir(dir):
         medianame = []
         for each in os.listdir(os.path.join(dir, tvlist)):  # 获取二级目录下的视频文件
-            mediaext = ['.mkv', '.mp4', '.ass', '.srt']  # 利用二级目录下的视频文件进行搜索（同时操作字幕）
+            mediaext = ['.mkv', '.mp4', '.ts', '.ass', '.srt']  # 利用二级目录下的视频文件进行搜索（同时操作字幕）
             if each.endswith(tuple(mediaext)):  # 获取二级目录下的nfo文件
                 medianame.append(each)
         find_adj_TV(medianame, tvlist, dir)  # 将获取到的文件名输入list,获取子文件夹名中的季元信息,并且进行文件名修改和移动
